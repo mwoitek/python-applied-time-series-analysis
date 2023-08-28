08cedc090c5a2fc3e264b53054bc5ea1a093f83f
Marcio Woitek <mwoitekjr@gmail.com>
2023-08-28 20:49:41 -0300

    Section 3: Random Walk

---
+++ section_3/random_walk.py
@@ -0,0 +1,50 @@
+# %% [markdown]
+# # Random Walk
+
+# %%
+import warnings
+
+import matplotlib.pyplot as plt
+import numpy as np
+from statsmodels.graphics.tsaplots import plot_acf
+from statsmodels.tsa.stattools import acf
+
+warnings.filterwarnings("ignore")
+
+# %%
+plt.rcParams["figure.figsize"] = [10, 7.5]
+
+# %% [markdown]
+# ## Simulation of Random Walk
+
+# %%
+steps = np.random.standard_normal(1000)
+steps[0] = 0
+
+random_walk = np.cumsum(steps)
+
+# %%
+random_walk[:10]
+
+# %%
+plt.plot(random_walk)
+plt.title("Simulated Random Walk")
+plt.show()
+
+# %%
+random_walk_acf_coef = acf(random_walk)
+random_walk_acf_coef
+
+# %%
+plot_acf(random_walk, lags=20, auto_ylims=True)
+
+# %%
+random_walk_diff = np.diff(random_walk, n=1)
+
+# %%
+plt.plot(random_walk_diff)
+plt.title("Noise")
+plt.show()
+
+# %%
+plot_acf(random_walk_diff, lags=20, auto_ylims=True)
